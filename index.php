71bc54f05274d262280c61d1df66a56e89f5125e
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-31 23:46:30 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$channels = [
+    "Safari TV" => "safari_tv",
+    "Victers TV" => "victers_tv",
+    "Flowers TV" => "flowers_tv",
+    "Manorama News" => "manorama_news",
+    "Aaj Tak" => "aaj_tak"
+];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IPTV Streaming</title>
+</head>
+<body>
+    <h2>Click a Channel to Play</h2>
+    <ul>
+        <?php foreach ($channels as $name => $id): ?>
+            <li><a href="stream.php?channel=<?= $id ?>">▶ <?= $name ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
