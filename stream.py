82271a9a69f5e20a5356149b3ada86c5f1b8e45f
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-04-01 04:38:04 -0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -61,4 +61,4 @@ def stream():
     return Response(generate(), content_type='video/3gpp')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0
+    app.run(debug=True, host='0.0.0.0', port=5000)
