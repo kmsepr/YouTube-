a96c17ab0340cdb66e16bf72b5b4980944181014
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-04-18 19:50:52 +0530

    Update restream.py

--- restream.py
+++ restream.py
@@ -115,5 +115,4 @@ def index():
     return f"<h3>Available Streams</h3><ul>{''.join(links)}</ul>"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8000)
